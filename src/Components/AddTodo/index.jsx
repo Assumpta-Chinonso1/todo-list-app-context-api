ff265f85629767cd274bc479fabf06dde89eb3e3
Celestine Assumpta <assumptachinonso42@gmail.com>
2024-08-26 16:24:33 +0100

    New styling

--- src/Components/AddTodo/index.jsx
+++ src/Components/AddTodo/index.jsx
@@ -14,11 +14,10 @@ const AddTodo = () => {
         
     }
   return (
-    <form onSubmit={submitHandler} className='flex flex-row gap-x-2 justify-between'>
+    <form onSubmit={submitHandler}>
            <input type="text"  value={text} placeholder='Add a new Todo' 
-           onChange={(e) => {setText(e.target.value)}}
-           className="form-input w-full rounded-md border shadow-sm p-4"/>
-           <button type="submit" className='w-32 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded'>AddTodo</button>
+           onChange={(e) => {setText(e.target.value)}}/>
+           <button type="submit">AddTodo</button>
     </form>
   )
 }
